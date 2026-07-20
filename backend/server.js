d3b9f6dd5b4e787e59f9c7e0421540458349115e
Samarjit Singh <samar17211@gmail.com>
2026-07-20 11:29:42 +0530

    Backend fixes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5174",
+  origin: "http://localhost:5173",
   credentials: true,
 }));
 app.use(cookieParser()); // enable cookie parsing
